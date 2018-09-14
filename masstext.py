205b322f15e727713b0201bdb1c93046752044f3
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-14 11:29:17 -0400

    close fd

--- masstext.py
+++ masstext.py
@@ -2,27 +2,29 @@ import os
 import smtplib
 import getpass
 from time import sleep
+from progress.bar import Bar
 
 with open("gateways.txt", "r") as f:
     gateways = f.readlines()
 
 with open("numbers.txt", "r") as f:
-    num_cout = len(f.readlines())
+    num_count = len(f.readlines())
 
 gmail_user = input("Gmail Username: ")
 gmail_password = getpass.getpass()
 sent_from = gmail_user
 subject = input("Please enter a subject: ")
+bar = Bar("Sending...", max=num_count)
 
 with open("message.txt", "r") as message_file:
     message = message_file.read().replace('\n', '')
 
 with open("numbers.txt", "r") as numbers:
     for num in [n.strip() for n in numbers.readlines()]:
-        f = open("sent.txt", "r")
-        if num in [a.strip() for a in f.readlines()]:
-            print("Skipping: {}".format(num))
-            continue
+        with open("sent.txt", "r") as f:
+            if num in [a.strip() for a in f.readlines()]:
+                print("Skipping: {}".format(num))
+                continue
         print("Sending to {}".format(num))
         for gateway in gateways:
             to = num + "@" + gateway
