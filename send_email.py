eeb7bc1e5e21c02c3e3cb33a0b8236809921b2d5
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-13 23:04:33 -0400

    Initial script (NEEDS WORK)

---
+++ send_email.py
@@ -0,0 +1,38 @@
+import os
+import smtplib
+from time import sleep
+
+with open("gateways.txt", "r") as f:
+    gateways = f.readlines()
+
+gmail_user = input("Please enter your Gmail email: ")
+gmail_password = input("Please enter your password: ")
+sent_from = gmail_user
+subject = input("Please enter a subject: ") 
+
+with open("message.txt", "r") as message_file:
+    message = message_file.read().replace('\n', '')
+
+with open("numbers.txt", "r") as numbers:
+    for num in [n.strip() for n in numbers.readlines()]:
+        f = open("sent.txt", "r")
+        if num in [a.strip() for a in f.readlines()]:
+            print("Skipping: {}".format(num))
+            continue
+        print("Sending to {}".format(num))
+        for gateway in gateways:
+            to = num + "@" + gateway
+            msg = 'From : {}\nTo: {}\nSubject: {}\n\n{}'.format(gmail_user, to, subject, message)
+            try:
+                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server.ehlo()
+                server.login(gmail_user, gmail_password)
+                server.sendmail(sent_from, to, msg)
+                server.close()
+                sent = True
+            except:
+                print('Something went wrong...')
+                sent = False
+            if sent:
+                with open("sent.txt", "a") as sent_file:
+                    sent_file.write("{}\n".format(num))
