b9f5ca698420ee408b3419fc97efceed0b0eda20
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-17 09:10:45 -0400

    masstext

--- masstext.py
+++ masstext.py
@@ -1,47 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import smtplib
 import getpass
-from time import sleep
 from progress.bar import Bar
 
-def main(contact_info):
-    num_count = len(contact_info)
+def main(contacts, message):
+    num_count = len(contacts)
     gmail_user = input("Gmail Username: ")
     gmail_password = getpass.getpass()
+    subject = input("Subject: ")
     sent_from = gmail_user
-    subject = input("Please enter a subject: ")
-    bar = Bar("Sending...", max=num_count)
+    progress_bar = Bar("Sending...", max=num_count)
 
-    with open("message.txt", "r") as message_file:
-        message = message_file.read().replace('\n', '')
+    with open("gateways.txt", "r") as gfile:
+        gways = [l.strip() for l in gfile.readlines()]
 
-    with open("numbers.txt", "r") as numbers:
-        for num in [n.strip() for n in numbers.readlines()]:
-            with open("sent.txt", "r") as f:
-                if num in [a.strip() for a in f.readlines()]:
+    gateways = {l.split(',')[1] : l.split(',')[0] for l in gways}
+
+    for (num, carrier) in contacts:
+        try:
+            with open("sent.txt", "r") as sent_file:
+                if num in [a.strip() for a in sent_file.readlines()]:
                     print("Skipping: {}".format(num))
                     continue
-            print("Sending to {}".format(num))
-            for gateway in gateways:
-                to = num + "@" + gateway
-                msg = 'From : {}\nTo: {}\nSubject: {}\n\n{}'.\
-                    format(gmail_user, to, subject, message)
-                try:
-                    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-                    server.ehlo()
-                    server.login(gmail_user, gmail_password)
-                    server.sendmail(sent_from, to, msg)
-                    server.close()
-                    sent = True
-                except:
-                    print('Something went wrong...')
-                    sent = False
-                if sent:
-                    with open("sent.txt", "a") as sent_file:
-                        sent_file.write("{}\n".format(num))
+        except IOError:
+            pass
+        print("Sending to {}...".format(num))
+        gateway = gateways[carrier]
+        recepient = num + "@" + gateway
+        msg = 'From : {}\nTo: {}\nSubject: {}\n\n{}'.\
+               format(gmail_user, recepient, subject, message)
+        try:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(gmail_user, gmail_password)
+            server.sendmail(sent_from, recepient, msg)
+            server.close()
+            sent = True
+        except:
+            print("Failed to send!")
+            sent = False
+        if sent:
+            with open("sent.txt", "a+") as sent_file:
+                sent_file.write("{}\n".format(num))
+            progress_bar.next()
 
 if __name__ == "__main__":
-    with open("numbers.txt", "r") as f:
-        num_count = len(f.readlines())
+    with open("contacts.txt", 'r') as cfile:
+        CLINES = [l.strip() for l in cfile.readlines()]
+        CONTACTS = [(l.split(',')[0], l.split(',')[1]) for l in CLINES]
 
-    main()
+    with open("message.txt", "r") as message_file:
+        MESSAGE = message_file.read().replace('\n', '')
+    main(CONTACTS, MESSAGE)
